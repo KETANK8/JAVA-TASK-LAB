6c81f7eeffe407f3f30c9002cd841fdac9a56577
KETANK8 <ketankumar126@gmail.com>
2022-07-22 14:51:12 +0530

    commit 22 july

---
+++ 22-07-2022/factorial.java
@@ -0,0 +1,24 @@
+/*
+Java Program to find the factorial of a given number
+by Ketan Kumar
+*/
+
+import java.util.Scanner;
+
+class factorial
+{
+public static void main(String args[])
+{
+	Scanner sc = new Scanner(System.in); //making an object of scanner class named sc
+	System.out.print("Enter the Number : ");
+	int num = sc.nextInt(); // storing input from user in variable num
+	System.out.println(" ");
+	int fact=1,i=2;
+	while(i<=num) //calculating factorial 
+	{
+		fact *= i;
+		i++;
+	}
+	System.out.println("Factorial of "+num+" is :"+fact);
+}
+}
