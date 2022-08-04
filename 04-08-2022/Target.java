a1cd3d69f34c17fd912815a26700ce4be797c13c
KETANK8 <ketankumar126@gmail.com>
2022-08-04 15:48:38 +0530

    lab day 4 Aug

---
+++ 04-08-2022/Target.java
@@ -0,0 +1,67 @@
+/*
+	Program to find all the possible two number in an array 
+	whose sum is equal to the target.
+*/
+
+// importing scanner utility to scan the user input
+import java.util.Scanner;
+
+//class1
+//Main class
+public class Target{
+	public static void main(String args[]){
+		
+		//creating scanner object named scan
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Size of array:");
+		int size = scan.nextInt();
+		
+		//creating array arr
+		int[] arr;
+		arr = new int[size];
+		System.out.println("Enter "+size+" elements :");	
+		
+		//for loop to store element in array
+		for(int i=0;i<size;i++){
+			arr[i]= scan.nextInt();
+		}
+		System.out.println("Enter the Sum of two number : ");
+		int target = scan.nextInt();
+		
+		//creating object of search class named find.
+		search find = new search();
+		System.out.println("-------------Result---------------");
+		
+		// calling Sum method from search class using find object
+		find.Sum(arr,size,target);
+		scan.close();
+	}
+}
+
+//class2 
+// creating class search to traverse the array and find all possible set of value
+class search{
+	public void Sum(int arr[],int size,int target){
+		int lenght = size;
+		int[] result;
+		
+		//creating array name result to store set of value in result array
+		result = new int[2];
+		
+		//for loop to traverse the array
+		for(int i=0;i<lenght;i++){
+			for(int j=i+1;j<lenght;j++){
+				// comparing if sum of two elements is equal to target
+				if(target == arr[i]+arr[j]){
+					//storing right value in result array
+					result[0]=arr[i];
+					result[1]=arr[j];
+					
+					// printing all the possible set of two value whose sum is equal to target
+					System.out.print("Element: ["+result[0]+","+result[1]+"]");
+					System.out.println("  Index :["+i+","+j+"]");
+				}
+			}
+		}
+	}
+}
---
+++ 04-08-2022/calculator.java
@@ -0,0 +1,123 @@
+/*
+	Program to perform calculation using addition,subtraction multiplication and division
+	method taking any no of arguments using varargs.
+*/
+
+// importing scanner utility to scan the user input
+import java.util.Scanner;
+
+//main class
+//creating class calculator
+class calculator{
+	
+	//method 1
+	//main method
+	public static void main(String args[]){
+		
+		//creating scanner object
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the no of element : ");
+		int size = scan.nextInt();
+		int arr[];
+		
+		//creating array object to store user input
+		arr = new int[size];
+		System.out.println("Enter "+size+" Numbers :");
+		
+		//storing values 
+		for(int i=0;i<size;i++){
+			arr[i] = scan.nextInt();
+		}
+		int start = arr[0];
+		System.out.println("Select Operation to perform Choose from the following :");
+		System.out.println("for Addition enter       : + \nfor Subtraction enter    : - \nfor Multiplication enter : * ");
+		System.out.println("for Division enter       : /");
+		//storing selection in character choice
+		char choice = scan.next().charAt(0);
+		
+		//using switch to perform user seleted operation
+		switch(choice){
+			case '+' : addition(arr);//calling addition method
+						break;
+			case '-' : subtraction(start,arr);//calling subtraction method
+						break;
+			case '*' : multiplication(arr);//calling multiplication method
+						break;
+			case '/' : division(start,arr);//calling division method
+						break;
+			default : System.out.println("Invalid selection!!");
+		}
+		scan.close();
+	}
+	
+	//method 2
+	//addition method to perform addition on any no of element using varargs
+	public static void addition(int ...args){
+		int result = 0;
+		System.out.print("Addition of elements :\n----------------\n");
+		
+		//adding values 
+		for(int x:args){
+			System.out.println(x+" ");
+			result +=x;
+		}
+		System.out.println("--------------\n"+result+"\n--------------");
+	}
+	
+	//method 3
+	//subtraction method to perform subtraction on any no of element using varargs	
+	public static void subtraction(int start,int ...args){
+		int result = 0;
+		System.out.println("Subtraction of elements:\n-----------------\n");
+		
+		//subtracting values
+		for(int x : args){
+			System.out.println(x+" ");
+			if(result != 0){
+				result -= x;
+			}
+			if (result == 0){
+				result = start;
+				start = 0;
+			}
+			
+		}
+		System.out.println("--------------\n"+result+"\n--------------");
+	}
+	
+	//method 4
+	//multiplication method to perform multiplication on any no of element using varargs
+	public static void multiplication(int ...args){
+		int result = 1;
+		System.out.print("Multiplication of elements :\n----------------\n");
+		
+		//multiplying values
+		for(int x:args){
+			System.out.println(x+" ");
+			result *= x;
+		}
+		System.out.println("--------------\n"+result+"\n--------------");
+	}
+	
+	//method 5
+	//division method to perform subtraction on any no of element using varargs	
+	public static void division(int start,int ...args){
+		int result = 0;
+		System.out.println("Division of elements:\n-----------------\n");
+		
+		//dividing values
+		for(int x : args){
+			System.out.println(x+" ");
+			if(result != 0){
+				result /= x;
+			}
+			if (result == 0){
+				result = start;
+				start = 0;
+			}
+			
+		}
+		
+		System.out.println("--------------\n"+result+"\n--------------");
+	}
+}
